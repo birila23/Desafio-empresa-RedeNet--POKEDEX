9cc59104b149ac9daee9e88012970588b292e5a2
Ana Beatriz <anabeatrizqueiroza@gmail.com>
2025-09-02 21:55:24 -0300

    Configurações iniciais do projeto

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+})
